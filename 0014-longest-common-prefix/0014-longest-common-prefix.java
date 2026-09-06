798fbdfbff47ef2ca08dadefc48d3b15997c919e
Mutyala Indira Mallarapu <mutyalaindiramallarapu@gmail.com>
2026-09-06 20:30:50 +0530

    Time: 1 ms (60.35%), Space: 42.8 MB (97.95%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        int n=strs.length;
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            min=Math.min(min,strs[i].length());
+        }
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<min;i++){
+            char ch=strs[0].charAt(i);
+            for(int j=1;j<n;j++){
+                if(ch!=strs[j].charAt(i)) return sb.toString();
+            }
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+}
